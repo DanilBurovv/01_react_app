6b6dce681c73fe6f483c715470b26b99f1ac9fcd
danil_burov <danilburov1550@gmail.com>
2023-09-14 17:13:12 +0300

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="mx-auto w-[1200px]">
+      <h1 className="text-3xl text-red-800 text-center">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
