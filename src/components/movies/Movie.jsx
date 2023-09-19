4e0fcfd970b523a35f33fb9dcef080493aea60db
danil_burov <danilburov1550@gmail.com>
2023-09-19 21:19:24 +0300

    ready 3. layout and images

--- src/components/movies/Movie.jsx
+++ src/components/movies/Movie.jsx
@@ -1,8 +1,11 @@
 
 
-const Movie = ({item}) => {
+const Movie = ({item, config}) => {
   return (
     <div className="mb-3 rounded-lg border-slate-200 border-2">
+      {config?.images?.base_url && <div>
+        <img className="w-full max-w-full shadow-md shadow-slate-600 mb-4" src={config.images.base_url + "w500" + item.poster_path} alt={item.title + "poster"} />
+      </div>}
         <h2 className="text-2xl px-3">{ item.title }</h2>
     </div>
   )
--- src/components/movies/MoviesList.jsx
+++ src/components/movies/MoviesList.jsx
@@ -4,33 +4,41 @@ import Movie from './Movie'
 const API_URL = 
     "https://api.themoviedb.org/3/discover/movie?api_key=65e043c24785898be00b4abc12fcdaae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
 
+const CONFIG_URL =
+    "https://api.themoviedb.org/3/configuration?api_key=65e043c24785898be00b4abc12fcdaae";
+
 const MoviesList = () => {
     const [movies, setMovies] = useState({});
-    const [error, setError] = useState(null);
+    const [config, setConfig] = useState({});
+
+    const getConfig = async () => {
+        try {
+            const res = await fetch(CONFIG_URL);
+            const config = await res.json();
+            setConfig(config)
+        } catch (err) {
+        }
+    }
 
     const getMovies = async () => {
-        setError(null)
         try {
             const res = await fetch(API_URL);
             const movies = await res.json();
             setMovies(movies)
         } catch (err) {
-            setError(err.message)
-            console.log(err);
         }
     }
 
     useEffect(() => {
         getMovies();
+        getConfig();
     }, [])
 
 
   return (
     <div className="grid grid-cols-4 gap-4">
-        {error && <h1 className="text-3xl text-red-800 text-center">Error</h1> }
-        {!error && 
-            movies?.results && 
-            movies.results.map((m) => <Movie key={m.id} item={m} />)}
+        { movies?.results && 
+            movies.results.map((m) => <Movie key={m.id} item={m} config={config} />)}
     </div>
   )
 }
